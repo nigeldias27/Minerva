7881709980e7196517275e1282e62687f14b7572
nigeldias27 <nigeldias27@yahoo.com>
2023-04-03 21:33:21 +0530

    Added About Us page

--- components/Footer.js
+++ components/Footer.js
@@ -3,16 +3,16 @@ export default function Footer() {
   return (
     <div className="w-full bg-footerBackground flex flex-col items-center py-8">
       <div className="flex flex-row pb-4">
-        <div className="bg-beigeText p-4 mx-2 rounded-full">
+        <div className="bg-beigeText p-4 mx-2 rounded-full hover:bg-hoverbeigeText">
           <BsInstagram color="#3C3635" />
         </div>
-        <div className="bg-beigeText p-4 mx-2 rounded-full">
+        <div className="bg-beigeText p-4 mx-2 rounded-full hover:bg-hoverbeigeText">
           <BsYoutube color="#3C3635" />
         </div>
-        <div className="bg-beigeText p-4 mx-2 rounded-full">
+        <div className="bg-beigeText p-4 mx-2 rounded-full hover:bg-hoverbeigeText">
           <BsLinkedin color="#3C3635" />
         </div>
-        <div className="bg-beigeText p-4 mx-2 rounded-full">
+        <div className="bg-beigeText p-4 mx-2 rounded-full hover:bg-hoverbeigeText">
           <BsTwitter color="#3C3635" />
         </div>
       </div>
--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ export default function Headers() {
     <div className="w-screen bg-greyBlack flex flex-row justify-around p-4">
       <div className="flex flex-row items-center">
         <Image className="h-xl" src={logo} />
-        <h1 className="text-beigeText text-xl font-merriweather pl-3">
+        <h1 className="text-beigeText text-xl font-merriweather pl-3 hover:text-hoverbeigeText">
           MINERVA
         </h1>
       </div>
@@ -21,7 +21,7 @@ export default function Headers() {
           <div className="absolute bg-greyBlack top-0 left-0 w-screen h-screen flex flex-col">
             <div className="flex justify-end p-12">
               <RxCross1
-                className="text-beigeText text-bold"
+                className="text-beigeText text-bold hover:text-hoverbeigeText"
                 size={24}
                 onClick={openHandler}
               />
@@ -29,33 +29,33 @@ export default function Headers() {
             <div className="h-full flex flex-col justify-evenly items-center">
               <Link
                 href={"/allnews"}
-                className="text-beigeText text-xl font-merriweather px-3"
+                className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
               >
                 News
               </Link>
               <Link
                 scroll={false}
                 href={"/#timeline"}
-                className="text-beigeText text-xl font-merriweather px-3"
+                className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
               >
                 Timeline
               </Link>
               <Link
                 scroll={false}
                 href={"/#featuringPES"}
-                className="text-beigeText text-xl font-merriweather px-3"
+                className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
               >
                 Featuring PES
               </Link>
               <Link
                 href={"/aboutUs"}
-                className="text-beigeText text-xl font-merriweather px-3"
+                className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
               >
                 About Us
               </Link>
               <Link
                 href={"/#contactus"}
-                className="text-beigeText text-xl font-merriweather px-3"
+                className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
               >
                 Contact Us
               </Link>
@@ -63,7 +63,7 @@ export default function Headers() {
           </div>
         ) : (
           <RxHamburgerMenu
-            className="text-beigeText text-bold"
+            className="text-beigeText text-bold hover:text-hoverbeigeText"
             size={24}
             onClick={openHandler}
           />
@@ -72,33 +72,33 @@ export default function Headers() {
       <div className="hidden sm:flex sm:flex-row sm:items-center">
         <Link
           href={"/allnews"}
-          className="text-beigeText text-xl font-merriweather px-3"
+          className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
         >
           News
         </Link>
         <Link
           scroll={false}
           href={"/#timeline"}
-          className="text-beigeText text-xl font-merriweather px-3"
+          className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
         >
           Timeline
         </Link>
         <Link
           scroll={false}
           href={"/#featuringPES"}
-          className="text-beigeText text-xl font-merriweather px-3"
+          className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
         >
           Featuring PES
         </Link>
         <Link
           href={"/aboutUs"}
-          className="text-beigeText text-xl font-merriweather px-3"
+          className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
         >
           About Us
         </Link>
         <Link
           href={"/#contactus"}
-          className="text-beigeText text-xl font-merriweather px-3"
+          className="text-beigeText text-xl font-merriweather px-3 hover:text-hoverbeigeText"
         >
           Contact Us
         </Link>
