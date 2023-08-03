39a3b3fa854a919b2f76fa217fa2c35e5dfbb309
nigeldias27 <nigeldias27@yahoo.com>
2023-08-03 20:56:00 +0530

    Header change

--- components/Header.js
+++ components/Header.js
@@ -25,61 +25,40 @@ export default function Headers(props) {
   const handleDrawerClose = () => {
     setDrawerOpen(false);
   };
-
+  /*                    props.dark
+                      ? localStorage.setItem("mode", "")
+                      : localStorage.setItem("mode", "dark");
+                    props.dark ? props.setDark(false) : props.setDark(true); */
   return (
     <div className={props.dark == true ? "dark" : ""}>
       <div className="w-screen bg-#FCF7FF dark:bg-greyBlack flex flex-row justify-between py-2 px-8 z-10">
         <div className="flex flex-row">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerOpen}
-          >
-            <CgMenuLeft className="text-greyBlack dark:text-white" />
-          </IconButton>
+          <div className="flex flex-row">
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerOpen}
+            >
+              <CgMenuLeft className="text-greyBlack dark:text-white" />
+            </IconButton>
 
-          <Drawer
-            anchor="left"
-            open={drawerOpen}
-            onClose={handleDrawerClose}
-            PaperProps={{
-              style: {
-                backgroundColor: "black",
-                width: "300px",
-              },
-            }}
-          >
-            <List>
-              <ListItem
-                sx={{
-                  justifyContent: "center",
-                  fontFamily: "var(--font-georgia)",
-                }}
-              >
-                <Link href={"/aboutUs"}>
-                  <ListItemButton
-                    sx={{ color: "white", fontFamily: "var(--font-georgia)" }}
-                  >
-                    <ListItemText primary="About Us" className="font-georgia" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem sx={{ justifyContent: "center" }}>
-                <Link href={"/aboutUs"}>
-                  <ListItemButton sx={{ color: "white" }}>
-                    <ListItemText primary="Our Team" />
-                  </ListItemButton>
-                </Link>
-              </ListItem>
-              <ListItem sx={{ justifyContent: "center" }}>
-                <Link
-                  href={""}
-                  onClick={() => {
-                    props.dark
-                      ? localStorage.setItem("mode", "")
-                      : localStorage.setItem("mode", "dark");
-                    props.dark ? props.setDark(false) : props.setDark(true);
+            <Drawer
+              anchor="left"
+              open={drawerOpen}
+              onClose={handleDrawerClose}
+              PaperProps={{
+                style: {
+                  backgroundColor: "black",
+                  width: "300px",
+                },
+              }}
+            >
+              <List>
+                <ListItem
+                  sx={{
+                    justifyContent: "center",
+                    fontFamily: "var(--font-georgia)",
                   }}
                 >
                   <Link href={"/aboutUs"}>
@@ -100,52 +79,59 @@ export default function Headers(props) {
                     </ListItemButton>
                   </Link>
                 </ListItem>
-                <ListItem sx={{ justifyContent: "center" }} className="sm:hidden">
+                <ListItem
+                  sx={{ justifyContent: "center" }}
+                  className="sm:hidden"
+                >
                   <Link href={"/allnews"}>
-                    <ListItemButton sx={{color: "white" }}>
+                    <ListItemButton sx={{ color: "white" }}>
                       <ListItemText primary="News" />
                     </ListItemButton>
                   </Link>
-                 </ListItem>
-                 <ListItem sx={{ justifyContent: "center" }} className="sm:hidden">
+                </ListItem>
+                <ListItem
+                  sx={{ justifyContent: "center" }}
+                  className="sm:hidden"
+                >
                   <Link href={"/#timeline"}>
-                    <ListItemButton sx={{color: "white" }}>
+                    <ListItemButton sx={{ color: "white" }}>
                       <ListItemText primary="Timeline" />
                     </ListItemButton>
                   </Link>
-                 </ListItem>
-                 <ListItem sx={{ justifyContent: "center" }} className="sm:hidden">
+                </ListItem>
+                <ListItem
+                  sx={{ justifyContent: "center" }}
+                  className="sm:hidden"
+                >
                   <Link href={"/#featuringPES"}>
-                    <ListItemButton sx={{color: "white" }}>
+                    <ListItemButton sx={{ color: "white" }}>
                       <ListItemText primary="Featuring PES" />
                     </ListItemButton>
                   </Link>
-                 </ListItem> 
-                 <ListItem sx={{ justifyContent: "center" }} className="sm:hidden">
+                </ListItem>
+                <ListItem
+                  sx={{ justifyContent: "center" }}
+                  className="sm:hidden"
+                >
                   <Link href={"/#contactus"}>
-                    <ListItemButton sx={{color: "white" }}>
+                    <ListItemButton sx={{ color: "white" }}>
                       <ListItemText primary="Contact Us" />
                     </ListItemButton>
                   </Link>
-                 </ListItem>
+                </ListItem>
                 <ListItem sx={{ justifyContent: "center" }}>
                   <Link
                     href={""}
                     onClick={() => {
-                      localStorage.getItem("mode") == "dark"
+                      props.dark
                         ? localStorage.setItem("mode", "")
                         : localStorage.setItem("mode", "dark");
+                      props.dark ? props.setDark(false) : props.setDark(true);
                     }}
                   >
                     <ListItemButton sx={{ color: "white" }}>
                       <ListItemText
-                        primary={
-                          typeof window !== "undefined"
-                            ? localStorage.getItem("mode") == "dark"
-                              ? "Light mode"
-                              : "Dark mode"
-                            : ""
-                        }
+                        primary={props.dark ? "Light mode" : "Dark mode"}
                       />
                     </ListItemButton>
                   </Link>
@@ -154,32 +140,33 @@ export default function Headers(props) {
             </Drawer>
           </div>
           <div className="hidden  sm:flex sm:flex-row sm:items-center">
-          <Link
-            href={"/allnews"}
-            className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
-          >
-            News
-          </Link>
-          <Link
-            scroll={false}
-            href={"/#timeline"}
-            className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
-          >
-            Timeline
-          </Link>
-          <Link
-            scroll={false}
-            href={"/#featuringPES"}
-            className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
-          >
-            Featuring PESU
-          </Link>
-          <Link
-            href={"/#contactus"}
-            className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
-          >
-            Contact Us
-          </Link>
+            <Link
+              href={"/allnews"}
+              className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
+            >
+              News
+            </Link>
+            <Link
+              scroll={false}
+              href={"/#timeline"}
+              className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
+            >
+              Timeline
+            </Link>
+            <Link
+              scroll={false}
+              href={"/#featuringPES"}
+              className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
+            >
+              Featuring PESU
+            </Link>
+            <Link
+              href={"/#contactus"}
+              className="text-#1D1D1D dark:text-white text-xl font-georgia px-8 hover:text-hoverbeigeText"
+            >
+              Contact Us
+            </Link>
+          </div>
         </div>
         <Link href={"/"} className="flex flex-row items-center">
           <Image
